d082250377d1e49764f00906d3892458d3c7abdc
silvi <silvi@LAPTOP-F7RFIJNR>
2024-04-07 18:38:02 +0200

    Versión 1.0

--- src/proyectodam/ProyectoDAM.java
+++ src/proyectodam/ProyectoDAM.java
@@ -15,6 +15,7 @@ public class ProyectoDAM {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Esto sale en pantalla");
     }
     
 }
